95edd7ce7ddd1e17c9270e903c38e8018037474f
Talha <Talha_Future@hotmail.com>
2021-10-20 18:41:34 +0300

    the player choice works fine now

--- scripts.js
+++ scripts.js
@@ -5,20 +5,20 @@ let player2= "";
 console.log(player1);
 console.log(player2);
 
-function computerPlay() {
+function computerSelection() {
     let numRPC = Math.floor(Math.random() * (2 - 0 + 1) + 0);
 
     switch(numRPC) {
         case 0:
-            player2 = "Rock";
+            player2 = "rock";
             break;
 
         case 1:
-            player2 = "Paper";
+            player2 = "paper";
             break;
 
         case 2:
-            player2 = "Scissors";
+            player2 = "scissors";
             break;
       } 
 
@@ -26,5 +26,41 @@ function computerPlay() {
 
 }
 
-computerPlay();
-console.log(player2)
+function playerSelection() {
+
+    // let playerChocie = prompt("Choose between Rock,Paper,Scissors");
+    // playerChocie = playerChocie.toString().toLowerCase().trim();
+
+    let isCorrect = false;
+    while(true){
+        playerChocie = prompt("Choose between Rock,Paper,Scissors");
+        playerChocie = playerChocie.toString().toLowerCase().trim();
+        
+        switch(playerChocie) {
+            case "rock":
+                 player1 = playerChocie;
+                 isCorrect = true;
+                 break;
+
+            case "paper":
+                player1 = playerChocie;
+                isCorrect = true;
+                break;
+        
+            case "scissors":
+                player1 = playerChocie;
+                isCorrect = true;
+                break;
+        }
+
+        if(isCorrect){
+            break;
+        }
+    }    
+}
+
+
+computerSelection();
+playerSelection();
+console.log(player1);
+console.log(player2);

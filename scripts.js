1dfe34e86b66b70cd2ab5d4d9b58fb7cca46eca1
Talha <Talha_Future@hotmail.com>
2021-10-20 18:11:12 +0300

    player2 is now able to choose randomly

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
 
-let player1 = "rock";
-let player2= "paper";
+let player1 = "";
+let player2= "";
 
 console.log(player1);
 console.log(player2);
 
 function computerPlay() {
-     player2 = "SSS";
+    let numRPC = Math.floor(Math.random() * (2 - 0 + 1) + 0);
+
+    switch(numRPC) {
+        case 0:
+            player2 = "Rock";
+            break;
+
+        case 1:
+            player2 = "Paper";
+            break;
+
+        case 2:
+            player2 = "Scissors";
+            break;
+      } 
+
      return;
 
 }

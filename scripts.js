8a702cc0dc6e51d04fc6f4f81541ede2fa14934d
Talha <Talha_Future@hotmail.com>
2021-10-20 17:58:48 +0300

    made sure that javascript works fine

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+let player1 = "rock";
+let player2= "paper";
+
+console.log(player1);
+console.log(player2);
+
+function computerPlay() {
+     player2 = "SSS";
+     return;
+
+}
+
+computerPlay();
+console.log(player2)
